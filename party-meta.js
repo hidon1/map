1e0f02e59c11c94fab2b061444a8524d5a832d08
zeev moyal <ZM0548407450@gmail.com>
2026-08-25 03:00:57 +0300

    Add external dashboard data asset

---
+++ party-meta.js
@@ -0,0 +1,91 @@
+/* Static visual identity for the polling dashboard. */
+window.POLLS_HEADER_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/The_knesset_illuminated.jpg/1280px-The_knesset_illuminated.jpg";
+
+window.PARTY_PROFILES = {
+  "ישר!": {
+    leader: "גדי איזנקוט",
+    monogram: "ישר",
+    color: "#00a2cc",
+    bloc: "opposition",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/6/61/Yashar_party_logo.png",
+    leaderImage: "https://ict.org.il/wp-content/uploads/2023/08/Eisenkot-Gadi_ICT-Team-Photo.png"
+  },
+  "הליכוד": {
+    leader: "בנימין נתניהו",
+    monogram: "מחל",
+    color: "#215ca3",
+    bloc: "netanyahu",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Likud_Logo.svg/512px-Likud_Logo.svg.png",
+    leaderImage: "https://upload.wikimedia.org/wikipedia/commons/7/74/Benjamin_Netanyahu%2C_February_2023.jpg"
+  },
+  "ביחד": {
+    leader: "נפתלי בנט",
+    monogram: "ביחד",
+    color: "#7354c7",
+    bloc: "opposition",
+    logo: "https://shortiougc.com/link-opengraph/go.be-yahad.org.il/MEJ9qejK9X6FTrrAa29Tm.png",
+    leaderImage: "https://upload.wikimedia.org/wikipedia/commons/9/96/Naftali_Bennett_official_portrait.jpg"
+  },
+  "הדמוקרטים": {
+    leader: "יאיר גולן",
+    monogram: "דמ",
+    color: "#d84c68",
+    bloc: "opposition",
+    logo: "https://pbs.twimg.com/profile_images/1940647288554029056/rz8xvIgJ.jpg",
+    leaderImage: "https://ict.org.il/wp-content/uploads/2022/08/MK-Yair-Golan_ICT-Website-Photo.png"
+  },
+  "ישראל ביתנו": {
+    leader: "אביגדור ליברמן",
+    monogram: "ל",
+    color: "#657789",
+    bloc: "opposition",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Israel-beytenu-logo.png",
+    leaderImage: "https://armenianweekly.com/wp-content/uploads/2024/04/Israeli-politician-Avigdor-Lieberman-2018-Wikimedia-Commons.jpg"
+  },
+  "עוצמה יהודית": {
+    leader: "איתמר בן גביר",
+    monogram: "כ",
+    color: "#ca721b",
+    bloc: "netanyahu",
+    logo: "https://crwflags.com/FOTW/images/i/il%7Doy-l.jpg",
+    leaderImage: "https://www.c-live.co.il/wp-content/uploads/2023/07/WhatsApp-Image-2023-07-05-at-19.36.59-e1688575285549.jpeg"
+  },
+  "יהדות התורה": {
+    leader: "יצחק גולדקנופף",
+    monogram: "ג",
+    color: "#303c76",
+    bloc: "netanyahu",
+    logo: "https://www.marefa.org/w/images/thumb/b/b4/United_Torah_Judaism_Logo_2019.svg/640px-United_Torah_Judaism_Logo_2019.svg.png",
+    leaderImage: "https://img.mako.co.il/2022/11/24/FF_YEHADUT_TORA_re_autoOrient_g.jpg"
+  },
+  "ש״ס": {
+    leader: "אריה דרעי",
+    monogram: "שס",
+    color: "#4665ae",
+    bloc: "netanyahu",
+    logo: "https://images.kikar.co.il/cdn-cgi/image/format%3Djpeg%2Cfit%3Dcontain%2Cwidth%3D900/2024/11/26/c74979f0-ac05-11ef-a142-d9c93479d225__h1156_w1200.png",
+    leaderImage: "https://www.israelhayom.co.il/wp-content/uploads/2021/01/15686112822275_b-960x640.jpg"
+  },
+  "הציונות הדתית": {
+    leader: "בצלאל סמוטריץ׳",
+    monogram: "ט",
+    color: "#34708b",
+    bloc: "netanyahu",
+    logo: "https://pbs.twimg.com/profile_images/1621096278334574594/nOA2cnIy_400x400.jpg",
+    leaderImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Bezalel_Smotrich.jpg/640px-Bezalel_Smotrich.jpg"
+  },
+  "רע״ם": {
+    leader: "מנסור עבאס",
+    monogram: "עם",
+    color: "#2d9869",
+    bloc: "arab",
+    leaderImage: "https://pbs.twimg.com/profile_images/1132965830474571776/xAx-HuJ4.jpg"
+  },
+  "חד״ש–תע״ל / המשותפת": {
+    leader: "איימן עודה",
+    monogram: "ודעם",
+    color: "#4e8a5b",
+    bloc: "arab",
+    leaderImage: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Ayman_Odeh_2021.png"
+  }
+};
